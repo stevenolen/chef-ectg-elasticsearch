2e88c6b8629a4e79a302292badc451ef6d50181a
Keith Rozett <krozett@gmail.com>
2021-10-16 18:13:28 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'technolengy@gmail.com'
 license 'apachev2'
 description 'Installs/Configures mwser-elasticsearch'
 long_description 'Installs/Configures mwser-elasticsearch'
-version '0.0.3'
+version '0.1.0'
 
 depends 'elasticsearch'
